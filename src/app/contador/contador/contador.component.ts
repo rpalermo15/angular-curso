2cfcbff32e19fdbd8503c07d9ed01d7264ca2778
Ricardo Palermo <rpalermo15@vera.com.uy>
2021-12-17 16:13:33 -0300

    vuelta a tras de prueba

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -19,7 +19,6 @@ export class ContadorComponent {
     titulo: string = 'Contador App';
     numero: number = 10;
     base  : number = 5;
-    pp: number = 1;
   
     acumular( valor: number ) {
         this.numero += valor;
